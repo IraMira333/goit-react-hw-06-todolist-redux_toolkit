f44bb9d9dac67f99e2b7deb8d29be36390b725eb
IraMira333 <irinat333@gmail.com>
2023-07-26 22:49:47 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,4 @@
+import { devToolsEnhancer } from '@redux-devtools/extension';
 import { createStore } from 'redux';
 
 const initialState = {
@@ -17,6 +18,8 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
   return state;
 };
+// Створюємо розширення стора, щоб додати інструменти розробника
+const enhancer = devToolsEnhancer();
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, enhancer);
 console.log(store.getState());
