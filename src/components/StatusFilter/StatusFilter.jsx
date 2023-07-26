4125aebcd907fa214f63de5020e81bd08bc75dc8
IraMira333 <irinat333@gmail.com>
2023-07-26 23:04:02 +0300

    Update StatusFilter.jsx

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -3,10 +3,11 @@ import css from './StatusFilter.module.css';
 import { useSelector } from 'react-redux';
 // Імпортуємо об'єкт значень фільтра
 import { statusFilters } from '../../redux/constants';
+import { getStatusFilter } from 'redux/selector';
 
 export const StatusFilter = () => {
   // Отримуємо значення фільтра із стану Redux
-  const filter = useSelector(state => state.filters.status);
+  const filter = useSelector(getStatusFilter);
   return (
     <div className={css.wrapper}>
       <Button selected={filter === statusFilters.all}>All</Button>
