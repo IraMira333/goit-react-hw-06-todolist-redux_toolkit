038954743168b993e9ca69c79fc5a1442f524b4a
IraMira333 <irinat333@gmail.com>
2023-07-28 11:48:52 +0300

    Update Task.jsx

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,9 +1,7 @@
-// Імпортуємо хук
 import { useDispatch } from 'react-redux';
-// Імпортуємо генератор екшену
-import { deleteTask, toggleCompleted } from '../../redux/actions';
 import { MdClose } from 'react-icons/md';
 import css from './Task.module.css';
+import { deleteTask, toggleCompleted } from 'redux/taskSlice';
 
 export const Task = ({ task }) => {
   // Отримуємо посилання на функцію відправки екшенів
